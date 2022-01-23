fde6294f2ef16baa4812dd3cdd2888819bfa98a6
蒋慧凯 <2656391651@qq.com>
2022-01-23 19:25:50 +0800

    assets配置文件修复

--- src/vue.config.js
+++
@@ -1,6 +0,0 @@
-// vue.config.js
-// var webpack = require('webpack');
-
-module.exports = {
-    assetsDir: 'static'  // for django,the assets url would changeto '/static/'
-}
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// vue.config.js
+// var webpack = require('webpack');
+
+module.exports = {
+    assetsDir: 'static'  // for django,the assets url would change to '/static/'
+}
