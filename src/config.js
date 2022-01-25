46aac5e7a4557c1dfc3341bbfa71af8b4f720ae9
蒋慧凯 <2656391651@qq.com>
2022-01-25 22:39:19 +0800

    修改config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://170.106.154.208:8000/';  // host:port
+export const BASE_URL = 'https://xuptnlp:8000/';  // host:port
+// export const BASE_URL = 'http://127.0.154.208:8000/';  // host:port
 
